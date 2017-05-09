874bb5a70891ac00c21b98bd78dd894c74baf0d6
Edmund Li <edmund.xz.lee@gmail.com>
2017-05-08 23:25:33 -0700

    uniq index migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509042043) do
+ActiveRecord::Schema.define(version: 20170509045732) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",      limit: 255
